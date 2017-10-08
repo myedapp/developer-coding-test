a49e0d8b67ed779f5465bbd13280e70848bcc703
Duy Nguyen <lthh89vt@gmail.com>
2017-10-08 15:42:43 +1100

    test: add simple test cases.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {BrowserModule} from "@angular/platform-browser";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        HttpModule,
+        FormsModule
       ],
     }).compileComponents();
   }));
@@ -17,16 +24,28 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Quest Pathways');
+  }));
+
+  it('should be able to handler null user id filter', async( () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.updateUserFilter(null);
+    expect(app.showingUserId).toEqual(null);
+  }));
+
+  it('getUserById() should return null for invalid userId', async( () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    let user;
+    const invalidUserList = [999, 'abc', null];
+    invalidUserList.map(invalidUserId => {
+      user = app.getUserById(invalidUserId);
+      expect(user).toBeNull();
+    });
   }));
 });
