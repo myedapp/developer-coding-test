129a72fea78d59f721506a515ff0c185b6f4c64e
Dinush Jayakody <dinushjay@gmail.com>
2018-04-17 16:17:41 +1000

    Added students factory and http calls

---
+++ services/studentsFactory.js
@@ -0,0 +1,24 @@
+(function(){
+
+    var studentsFactory = function($http){
+
+        var factory = {};
+
+        factory.getStudents = function () {
+            return $http.get('../users.json');
+        };
+
+        factory.getPathways = function() {
+            return $http.get('../quest_pathways.json');
+        }
+
+        return factory;
+    } ;
+
+    studentsFactory.$inject = ['$http'];
+
+    angular.module('studentsApp')
+        .factory('studentsFactory', studentsFactory);
+
+
+}());
