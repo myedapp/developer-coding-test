b561726d92bc462e9c8191b48e07adb9afea32c1
Dinush Jayakody <dinushjay@gmail.com>
2018-04-17 15:51:51 +1000

    Added controller and template for routing

--- app.js
+++ app.js
@@ -3,4 +3,13 @@
 
     var studentsApp = angular.module('studentsApp', ['ngRoute']);
 
+    studentsApp.config(function($routeProvider){
+        $routeProvider
+            .when('/',{
+                controller: 'StudentsController',
+                templateUrl: 'views/students.html'
+            })
+            .otherwise({ redirectTo: '/'});
+    });
+
 }())
