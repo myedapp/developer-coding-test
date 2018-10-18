3e82c783d312762b3f263a30e71deb0bfbb15da1
Anthony Smith <anthony.smith.01@gmail.com>
2018-10-19 07:36:03 +1100

    App initial constructor and function setup

--- myedquests/src/App.js
+++ myedquests/src/App.js
@@ -1,7 +1,35 @@
+/**
+ * @file provides an application for teachers to visualise student quests.
+ * @author Anthony Smith
+ * @version 1.0 
+ */
+
 import React, { Component } from 'react';
 import './App.css';
 
+/** Class that represents a simple application for viewing student quests. */
 class App extends Component {
+  
+  /**
+   * Creates the app with a state for users and quests.
+  */
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      quests: []
+    };
+  }
+
+  /**
+   * Fetches the user and quest data.
+  */
+  componentDidMount() {
+  }
+
+  /**
+   * Renders the user and quest data using imported components.
+  */
   render() {
   }
 }
