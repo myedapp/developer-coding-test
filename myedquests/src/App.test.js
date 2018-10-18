21ca84265090a89901f1359d5d8b7f47b1a63eea
Anthony Smith <anthony.smith.01@gmail.com>
2018-10-19 08:33:43 +1100

    Test Information

--- myedquests/src/App.test.js
+++ myedquests/src/App.test.js
@@ -1,6 +1,17 @@
+/**
+ * @file provides simple tests for the React application.
+ * @author Anthony Smith
+ * @version 1.0 
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+/** TODO: Normally I would provide more JestJs tests here. I've succesfully used JestJs
+ * setups such as nodejs etc. React, however, is a newer library to me so I am not fully familiar with
+ * unit testing these components. This is an area I am currently upskilling in.
+ */
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
