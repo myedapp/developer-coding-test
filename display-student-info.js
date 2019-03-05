b9cfe1abf2eaa9b42c96bab1b58129de11b50af5
Sahil Jaidka <sjaidka@deakin.edu.au>
2019-03-05 17:53:30 +1100

    MyEdOnline Recruitment Test

---
+++ display-student-info.js
@@ -0,0 +1,63 @@
+window.studentMarks = [];
+
+function displayAllStudents () {
+
+  $.get('/getStudents', function(result) {
+
+    var table = document.getElementById("display-student-info-table");
+
+    for (var i = 0; i < result.length; i ++) {
+
+      console.log (result[i].fullname);
+
+      var cell = table.insertRow (i);
+      var cell1 = cell.insertCell(0);
+      cell1.innerHTML = result[i].fullname;
+
+      var cell2 = cell.insertCell(1);
+
+      var nestedTable = document.getElementById("display-student");
+      var newTable = "<table style='width: 100%'>";
+      newTable += "<thead><tr><th>Quest</th><th>Prorgress</th><th>Marks</th>"
+      for (var j = 0; j < studentMarks.length; j++) {
+
+        if (result[i].id == studentMarks[j].user_id) {
+
+          for (var k = 0; k < studentMarks[j].quest_paths.length; k++) {
+
+            var questName = studentMarks[j].quest_paths[k].quest.name;
+            let quesMarkText = 'N/A';
+            if (studentMarks[j].quest_paths[k].mark.mark) {
+                quesMarkText = studentMarks[j].quest_paths[k].mark.mark;
+            }
+            let completionRate = 0;
+            if (studentMarks[j].quest_paths[k].mark.completion) {
+                completionRate = studentMarks[j].quest_paths[k].mark.completion + "%";
+            }
+              newTable += "<tr><td>"+questName+"</td><td>"+completionRate+"</td><td>"+quesMarkText+"</td></tr>";
+
+          }
+
+          break;
+        }
+      }
+        newTable += "</table>"
+        cell2.innerHTML = newTable;
+
+    }
+
+  })
+
+}
+
+
+function getStudentMarks() {
+
+  $.get('/getStudentData', function(result) {
+    studentMarks = result;
+
+    displayAllStudents ();
+
+  });
+
+}
---
+++ index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+//Require Hapi
+const Hapi = require ('hapi');
+const Path = require('path');
+
+//Create new Hapi server object with a host and a port number to listen on
+const server = Hapi.server ({
+
+  port: 3000,
+  host: 'localhost'
+
+});
+
+server.route({
+  method: 'GET',
+  path: '/getStudents',
+  handler: (request, reply) => {
+    
+    return parseDataFromAFile("./Data-Files/users.json");
+
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/getStudentData',
+  handler: (request, reply) => {
+
+    return parseDataFromAFile("./Data-Files/quest_pathways.json");
+
+  }
+});
+
+
+function parseDataFromAFile (filePath) {
+
+  var fs = require("fs");
+  var contents = fs.readFileSync(filePath);
+  var jsonContent = JSON.parse(contents);
+  return jsonContent;
+
+}
+
+
+const init = async () => {
+
+  await server.register(require('inert'));
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+
+      return h.file('index.html');
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/display-student-info.js',
+    handler: (request, h) => {
+
+      return h.file('display-student-info.js');
+    }
+  });
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+
+}
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+init();
