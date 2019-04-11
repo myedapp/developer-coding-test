076f65cf0880fb9a249be2c905b6b79df768b989
ruegen <ruegen@icloud.com>
2019-04-11 20:17:48 +1000

    update css

--- frontend/src/components/users/UserList.js
+++ frontend/src/components/users/UserList.js
@@ -1,12 +1,17 @@
 import React from "react"
 import "./UserList.scss"
-const UserList = ({ users }) => {
+
+const UserList = ({ users, handleClick, selectedId }) => {
+  const active = id => {
+    return `user-info-button ${selectedId === id ? "active" : ""}`
+  }
   return (
     <ul className="user-list">
       {users.map(({ id, fullname }) => (
         <li key={id}>
-          {fullname}
-          <button className="button-info">i</button>
+          <button onClick={() => handleClick(id)} className={active(id)}>
+            {fullname}
+          </button>
         </li>
       ))}
     </ul>
