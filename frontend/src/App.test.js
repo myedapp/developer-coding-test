88fdeaf42c3ec4bbdaa5211173bbd3bbc9aa8b96
ruegen <ruegen@icloud.com>
2019-04-12 00:55:38 +1000

    add react test tools

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { act } from "react-dom/test-utils"
+import { act } from "react-testing-library"
 import { shallow } from "enzyme"
 import App from "./App"
 import users from "./components/users/users.json"
@@ -16,15 +16,21 @@ describe("Component: App", () => {
   /*
    THIS WOULD WORK BUT FACEBOOK HASN'T PATCHED THE BUG YET UNTIL NEXT RELEASE XD
   */
-  // it("renders without crashing", () => {
-  //   fetch.mockResponse(JSON.stringify([...users]))
-  //   const div = document.createElement("div")
-  //   act(() => {
-  //     ReactDOM.render(<App />, div)
-  //   })
+  it("renders without crashing", () => {
+    fetch.mockResponse(JSON.stringify([...users]))
+    const div = document.createElement("div")
+    act(() => {
+      ReactDOM.render(<App />, div)
+    })
 
-  //   act(() => {
-  //     ReactDOM.unmountComponentAtNode(div)
-  //   })
-  // })
+    act(() => {
+      ReactDOM.unmountComponentAtNode(div)
+    })
+  })
+
+  it("loads users", () => {
+    fetch.mockResponse(JSON.stringify([...users]))
+    const wrapper = shallow(<App />)
+    expect(wrapper.state().users.length).toBe(5)
+  })
 })
