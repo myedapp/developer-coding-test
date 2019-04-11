706f6077a03fc53def561998b00740d786e22dc1
ruegen <ruegen@icloud.com>
2019-04-12 00:29:35 +1000

    add spinner to user list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from "react"
 import UserList from "./components/users/UserList"
 import UserInfo from "./components/users/UserInfo"
+import { MoonLoader } from "react-spinners"
 
 import "./App.scss"
 const App = () => {
   const [users, setUsers] = useState([])
   const [userId, setUserId] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [defer, setDefer] = useState(null)
 
   useEffect(() => {
+    setIsLoading(true)
     fetch("http://localhost:3000/users")
       .then(res => res.json())
       .then(users => {
         setUsers(users)
+        setIsLoading(false)
       })
       .catch(err => {
         console.error(err)
@@ -20,6 +25,17 @@ const App = () => {
 
   const selectUser = id => {
     setUserId(id)
+  }
+
+  if (isLoading) {
+    return (
+      <aside className="user-aside">
+        <h2>Students</h2>
+        <div className="loader">
+          <MoonLoader color="white" size={40} />
+        </div>
+      </aside>
+    )
   }
 
   const user = userId ? users.find(u => u.id === userId) : null
