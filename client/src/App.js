acbfc36cca7a7b96dad9bbba50853b198a1a1b19
Ryan Zhang <ryanzhangau@gmail.com>
2019-04-16 21:28:50 +1000

    load student component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import List from './components/Student/List';
 
 class App extends Component {
+  state = {
+    students: [],
+    quests: []
+  };
+
+  componentDidMount() {
+    const promise = [];
+    promise.push(fetch('api/users').then(res => res.json()));
+    promise.push(fetch('api/quests').then(res => res.json()));
+    Promise.all(promise).then(res =>
+      this.setState({ students: [...res[0].data], quests: [...res[1].data] })
+    );
+  }
+
   render() {
     return (
       <div className='App'>
         <Header />
+        <List items={this.state.students} tagName='Student' />
       </div>
     );
   }
