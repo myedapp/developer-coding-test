fc04659042cc3639700e36ef506e3858543ac58e
Ryan Zhang <ryanzhangau@gmail.com>
2019-04-16 19:42:59 +1000

    inject Header into App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
-    return <div className='App'>code here</div>;
+    return (
+      <div className='App'>
+        <Header />
+      </div>
+    );
   }
 }
 
